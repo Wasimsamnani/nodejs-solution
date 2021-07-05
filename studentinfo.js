064f6a93df9721d4b93e34362e21b940d0e3e8cc
wasim samnani <wasimsamnani110@gmail.com>
2021-07-05 15:42:43 +0530

    second commit

--- studentinfo.js
+++ studentinfo.js
@@ -1,14 +1,8 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const mysql = require('mysql');
 require('dotenv').config();
 var app = express();
-
-app.use(bodyParser.urlencoded({
-   extended: true
-}));
-
-app.use(bodyParser.json());
+app.use(express.json())
 
 var sql = mysql.createConnection({
   host:"localhost",
@@ -17,32 +11,70 @@ var sql = mysql.createConnection({
   password:process.env.PASS
 })
 
-app.get('/',(req,res) => {
-  res.sendFile(__dirname+"/studentinfo.html");
-});
-
-app.post('/Store',(req,res) => {
+app.post('/signup',(req,res) => {
   console.log(req.body);
   let {rollno,name,address,course,mobilenumber,gender,email} = req.body;
   if(!rollno || !name||!address||!course||!mobilenumber||!gender||!email){
-    res.write("plz enter all fields");
-    res.end();
+    res.send({message:"plz enter all fields"});
   }else {
 
     let q ="insert into studentinfo values('"+rollno+"','"+name+"','"+address+"','"+course+"','"+mobilenumber+"','"+gender+"','"+email+"')";
     sql.query(q,(err,r) => {
       if (err) {
-        res.write(err.toString())
+        res.json({message:err.toString()})
         res.end();
       }
        else{
-        res.write("done with storing data");
+        res.send({message:"done with storing data"});
         res.end();
         console.log(r);
       }
       });
   }
 
+
 })
-app.listen(8080);
-console.log("connected to port 3000");
+app.get('/details',(req,res) => {
+  sql.query("select * from studentinfo",(err,r) => {
+    if (err) {
+      res.json({message:err.toString()});
+      res.end();
+    }
+      else{
+        res.json({result:r})
+      }
+  })
+})
+
+app.post('/delete',(req,res) => {
+  let q='delete from studentinfo where rollno=?'
+  sql.query(q,[req.body.rollno],(err) => {
+    if(err){
+      res.send({message:err.toString()})
+    }
+    else {
+      res.send({message:"done deleting"})
+    }
+  })
+})
+
+app.post('/update',(req,res) => {
+  console.log(req.body);
+  let {id,rollno,name,address,course,mobilenumber,gender,email} = req.body;
+  if(!rollno || !name||!address||!course||!mobilenumber||!gender||!email){
+    res.send({message:"plz enter all fields"});
+  }else {
+
+    let q = "update studentinfo set rollno="+rollno+",name='"+name+"',address='"+address+"',course='"+course+"',mobilenumber='"+mobilenumber+"',gender='"+gender+"',email='"+email+"' where rollno="+id+"";
+    sql.query(q,(err) => {
+      if (err) {
+        res.send({message:err.toString()});
+      }
+      else{
+        res.send({message:"updated"});
+      }
+  })
+}
+})
+app.listen(8000);
+console.log("connected to port 8000");
